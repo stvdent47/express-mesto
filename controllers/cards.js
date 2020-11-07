4df257991e740f0bd9ec2426e78dda96f2b0ad6b
Anton Rodionov <stvdent47@gmail.com>
2020-11-08 00:38:25 +0300

    finished the fixes

--- controllers/cards.js
+++ controllers/cards.js
@@ -7,21 +7,6 @@ module.exports.getCards = (req, res) => {
   readFile(pathToCards)
     .then((data) => res.send(data))
     .catch(() => {
-      res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
-    });
-};
-
-module.exports.getCard = (req, res) => {
-  const { id } = req.params;
-  readFile(pathToCards)
-    .then((data) => {
-      const card = data.find((item) => item._id === id);
-      if (!card) {
-        res.status(404).send({ message: 'Нет карточки с таким id' });
-      }
-      res.send(card);
-    })
-    .catch(() => {
-      res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+      res.status(500).send({ message: 'Запрашиваемый ресурс не найден' });
     });
 };
